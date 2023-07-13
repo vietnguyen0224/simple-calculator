a20d9ef4a208e6a3ef6f59ae7f599cca843511eb
Viet Nguyen <nguyenhoangviet24022@gmail.com>
2023-07-12 23:43:06 -0500

    Finished projectgit add .!

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ function divide(a,b) {
 
 function operate(operator, n1, n2) {
   switch (operator) {
-    case 'add':
+    case '+':
       display.textContent = add(n1,n2);
       break;
-    case 'subtract':
+    case '-':
       display.textContent = subtract(n1,n2);
       break;
-    case 'multiply':
+    case '*':
       display.textContent = multiply(n1,n2);
       break;
-    case 'divide':
+    case '/':
       display.textContent = divide(n1,n2);
       break;
     default:
@@ -48,6 +48,35 @@ function save(num, text, o) {
   saved.textContent = n1 + " " + text;
 }
 
+// Handle chain operators
+function chain(text, o) {
+  if (!n1) {
+    save(display.textContent, text, o);
+    return;
+  };
+  if (n1 && !n2) {
+    n2 = display.textContent;
+    saved.textContent += " " + n2;
+    operate(operator, n1, n2); 
+    if (display.textContent === 'ERROR!!! Divisor can\'t be ZERO!!!') {
+      return;
+    }
+    save(display.textContent, text, o);
+    n2 = null;
+    return;
+  }
+}
+
+// Handle equal operator
+function result() {
+  document.querySelectorAll('.number').forEach(num => num.disabled = true);
+  document.querySelectorAll('.operator').forEach(o => o.disabled = true);
+  document.getElementById('delete').disabled = true;
+  n2 = display.textContent;
+  saved.textContent += " " + n2;
+  operate(operator, n1, n2);
+}
+
 onclick = event => {
   if (event.target.className === 'number') {
     if (event.target.id === '.') document.getElementById('.').disabled = true;
@@ -56,30 +85,15 @@ onclick = event => {
 
   if (event.target.className === 'operator') {
     document.getElementById('.').disabled = false;
-    if (!n1) {
-      save(display.textContent, event.target.textContent, event.target.id);
-      return;
-    };
-    if (n1 && !n2) {
-      n2 = display.textContent;
-      saved.textContent += " " + n2;
-      operate(operator, n1, n2); 
-      if (display.textContent === 'ERROR!!! Divisor can\'t be ZERO!!!') {
-        return;
-      }
-      save(display.textContent, event.target.textContent, event.target.id);
-      n2 = null;
-      return;
-    }
+    chain(event.target.textContent, event.target.id)
   }
 
   if (event.target.id === '=') {
     event.target.disabled = true;
-    document.querySelectorAll('.number').forEach(num => num.disabled = true);
-    document.querySelectorAll('.operator').forEach(o => o.disabled = true);
-    n2 = display.textContent;
-    saved.textContent += " " + n2;
-    operate(operator, n1, n2); 
+    result();
+  }
+  if (event.target.id === 'delete') {
+    del();
   }
 }
 
@@ -89,24 +103,24 @@ onkeydown = event => {
     document.getElementById('.').disabled = true;
     show(document.getElementById(event.key));
   }
-  if (event.key === '+' || event.key === '-' || event.key === '*') {
+  if (event.key === '+' || event.key === '-' || event.key === '*' || event.key === '/') {
     document.getElementById('.').disabled = false;
-    if (!n1) {
-      save(display.textContent, event.key, event.key);
-      return;
-    };
-    if (n1 && !n2) {
-      n2 = display.textContent;
-      saved.textContent += " " + n2;
-      operate(operator, n1, n2); 
-      if (display.textContent === 'ERROR!!! Divisor can\'t be ZERO!!!') {
-        return;
-      }
-      save(display.textContent, event.key, event.key);
-      n2 = null;
-      return;
-    }
+    chain(event.key, event.key);
   }
+  if (event.key === '=') {
+    document.getElementById('=').disabled = true;
+    result();
+  }
+  if (event.key === 'Backspace' || event.key === 'Delete') {
+    del();
+  }
+}
+
+// Handle delete btn
+function del() {
+  display.textContent = display.textContent.slice(0,-1);
+  if (!display.textContent.includes('.')) document.getElementById('.').disabled = false;
+  if (display.textContent === '') display.textContent = '0';
 }
 
 // Handle clear btn
@@ -118,4 +132,5 @@ function clear() {
   document.getElementById('=').disabled = false;
   document.querySelectorAll('.number').forEach(num => num.disabled = false);
   document.querySelectorAll('.operator').forEach(o => o.disabled = false);
+  document.getElementById('delete').disabled = false;
 }
